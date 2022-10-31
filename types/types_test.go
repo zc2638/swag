b28521eb84d440e047584a816cecf64e494c28bb
zc <zc2638@qq.com>
2022-10-31 12:36:09 +0800

    add coverage CI (#2)
    add coverage ci

--- types/types_test.go
+++ types/types_test.go
@@ -4,6 +4,7 @@ package types
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,6 +17,55 @@ func TestParameterType(t *testing.T) {
 	assert.Equal(t, "string", String.String())
 	assert.Equal(t, "array", Array.String())
 	assert.Equal(t, "file", File.String())
+}
+
+func TestParameterType_String(t *testing.T) {
+	tests := []struct {
+		name string
+		pt   ParameterType
+		want string
+	}{
+		{
+			name: "integer",
+			pt:   Integer,
+			want: "integer",
+		},
+		{
+			name: "number",
+			pt:   Number,
+			want: "number",
+		},
+		{
+			name: "boolean",
+			pt:   Boolean,
+			want: "boolean",
+		},
+		{
+			name: "string",
+			pt:   String,
+			want: "string",
+		},
+		{
+			name: "array",
+			pt:   Array,
+			want: "array",
+		},
+		{
+			name: "file",
+			pt:   File,
+			want: "file",
+		},
+		{
+			name: "unknown",
+			pt:   ParameterType("unknown"),
+			want: "unknown",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, tt.pt.String(), "String()")
+		})
+	}
 }
 
 func TestAddURLParamsToContext(t *testing.T) {
@@ -82,6 +132,100 @@ func TestAddURLParamsToContext(t *testing.T) {
 				return
 			}
 			assert.Equalf(t, tt.want, routeCtx.PathParams, "AddURLParamsToContext(%v, %v)", tt.args.parent, tt.args.params)
+		})
+	}
+}
+
+func TestURLParam(t *testing.T) {
+	req, _ := http.NewRequest(http.MethodGet, "/", nil)
+	ctx := AddURLParamsToContext(req.Context(), map[string]string{
+		"name": "zc",
+	})
+	req = req.WithContext(ctx)
+
+	type args struct {
+		r   *http.Request
+		key string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "no ctx",
+			args: args{
+				r:   new(http.Request),
+				key: "none",
+			},
+			want: "",
+		},
+		{
+			name: "no value",
+			args: args{
+				r:   req,
+				key: "none",
+			},
+			want: "",
+		},
+		{
+			name: "value exists",
+			args: args{
+				r:   req,
+				key: "name",
+			},
+			want: "zc",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, URLParam(tt.args.r, tt.args.key), "URLParam(%v, %v)", tt.args.r, tt.args.key)
+		})
+	}
+}
+
+func TestURLParamFromCtx(t *testing.T) {
+	ctx := AddURLParamsToContext(context.Background(), map[string]string{
+		"name": "zc",
+	})
+
+	type args struct {
+		ctx context.Context
+		key string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "no ctx",
+			args: args{
+				ctx: nil,
+				key: "none",
+			},
+			want: "",
+		},
+		{
+			name: "no value",
+			args: args{
+				ctx: ctx,
+				key: "none",
+			},
+			want: "",
+		},
+		{
+			name: "value exists",
+			args: args{
+				ctx: ctx,
+				key: "name",
+			},
+			want: "zc",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, URLParamFromCtx(tt.args.ctx, tt.args.key), "URLParamFromCtx(%v, %v)", tt.args.ctx, tt.args.key)
 		})
 	}
 }
